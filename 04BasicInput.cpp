d8576669357b1b8ff9a4070c220055adfa2acd5b
jenise-maligat <jenisemaligat@gmail.com>
2024-10-03 13:15:21 +0800

    https://github.com/jenise-maligat/CC102/upload/main

---
+++ 04BasicInput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Nickname\t\t|: JEN" << endl;
+	cout << "Course-Year\t\t|: 	BSIT-1" << endl;
+	cout << "school\t\t|: OLLCF-DAET" << endl;
+	return 0;
+}
+	
