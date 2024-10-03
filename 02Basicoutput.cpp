7cecc7a8b4af8365b8397facdad0718d730ac5c1
jenise-maligat <jenisemaligat@gmail.com>
2024-10-03 12:58:39 +0800

    https://github.com/jenise-maligat/CC102/upload/main

---
+++ 02Basicoutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "school\t\t\t| Our Lady of lourdes college foundation" << end;
+	cout << "Department\t\t\t| Computer studies" << end;
+	cout << "course & Year Level\t\t| BSIT 1st Year" << end;
+	cout << "Your Permanent Address\t\t| Bicutan Taguig" << end;
+	
+    return 0;
+
+}
