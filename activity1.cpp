d664bb08f844abdf74c6133687e7475b7029ecdc
jenise-maligat <jenisemaligat@gmail.com>
2024-11-13 10:03:04 +0800

    Add files via upload

---
+++ activity1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+	int temperature;
+	cout << "Enter the temperature: ";
+	cin >> temperature;
+	
+	if (temperature <= 32) {
+		cout << "Bring a heavy jacket." <<endl;
+	}
+	else if (temperature >= 32 && temperature  <50) {
+		cout << "Bring a light jacket." <<endl;
+	}
+	if (temperature >= 50) {
+			cout << "No jacket needed.";
+	}
+	return 0;
+}
