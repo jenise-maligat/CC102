887d734cca5cce1a05d24b6c9f8ae588af2912f7
jenise-maligat <jenisemaligat@gmail.com>
2024-11-19 09:32:39 +0800

    Add files via upload

---
+++ ageoptimize2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+      int age;
+	  cout << "enter your age?: ";
+	  cin >> age;
+	  if (age<16) 
+		  cout << "too young to drive. ";
+	else if(age==16)
+	      cout << "better clear the road. "; 
+	else  if(age>16)
+	      cout << "you are getting pretty old. " ;
+	return 0:
+}
