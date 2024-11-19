a7d1fbfc8255f4b436bf8648bf35d2deaa195be4
jenise-maligat <jenisemaligat@gmail.com>
2024-11-19 15:34:26 +0800

    Add files via upload

---
+++ price.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	float sugarPriceUSD = 0.00, ricePricePound = 0.00, sardinesPricePound = 0.00, coffeePriceUSD = 0.00, milkPriceUSD = 0.00;
+	cout << "Enter the Price of Sugar: ";
+	cin >> sugarPriceUSD;
+	cout << "Enter the Price of Rice: ";
+	cin >> ricePricePound;
+	cout << "Enter the Price of Sardines: ";
+	cin >> sardinesPricePound;
+	cout << "Enter the Price of Coffee: ";
+	cin >> coffeePriceUSD;
+	cout << "Enter the Price of Milk: ";
+	cin >> coffeePriceUSD;
+	cout << "-------------------------------------------------------------------------------------------------------------------";
+	cout << "Enter the Quantity of Sugar: ";
+	cin >> sugarQty;
+	cout << "Enter the Quantity of rice: ";
+	cin >> riceQty;
+	cout << "Enter the Quantity of sardines: ";
+	cin >> sardinesQty;
+	cout << "Enter the Quantity of coffee: ";
+	cin >> coffee;
+	cout << "Enter the Quantity of milk: ";
+	cin << milkQty
+	return 0;
+
+}
