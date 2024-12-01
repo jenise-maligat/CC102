d10c58571d1ca67d55c7295cb60cedadc4f62faf
jenise-maligat <jenisemaligat@gmail.com>
2024-12-01 09:25:52 +0800

    Create loopingE.cpp

---
+++ loopingE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter number of rows: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int k = n-i; k > 0; k--) {
+			cout << " ";
+		}
+		for (int j = 1; j <= i; j++) {
+			cout << j;
+		}
+		cout << endl;
+	}
+	return 0;
+}
